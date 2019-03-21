4c6826ec5cad0175aeece7f7b7c672a71ea78a9d
NdeWs <nate@natedewaard.com>
2019-03-21 15:35:51 -0400

    changing models as I go and admin display

--- gematria/gematriacore/admin.py
+++ gematria/gematriacore/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
-
+from gematria.gematriacore.models import(
+    LetterPower,
+    LetterMeaning,
+    Letter,
+    WordMeaning,
+    Word,
+    Dictionary,
+    Alphabet,
+    Language,
+    GematriaMethodLetterRule,
+    GematriaMethod
+)
 # Register your models here.
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Alphabet)
+class AlphabetAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Dictionary)
+class DictionaryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WordMeaning)
+class WordMeaningAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Letter)
+class LetterAdmin(admin.ModelAdmin):
+    filter_horizontal = ('powers', 'meanings')
+    search_fields = ['title', 'character']
+    list_display = ['title', 'character', 'alphabet']
+    list_filter = ['alphabet']
+
+@admin.register(LetterMeaning)
+class LetterMeaningAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(LetterPower)
+class LetterPowerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(GematriaMethod)
+class GematriaMethodAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(GematriaMethodLetterRule)
+class GematriaMethodLetterRuleAdmin(admin.ModelAdmin):
+    pass
--- gematria/gematriacore/models/models.py
+++ gematria/gematriacore/models/models.py
@@ -13,18 +13,79 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-class Language(TimeStampedModel):
-    title = models.CharField(max_length=200)
+class LetterPower(TimeStampedModel):
+    """
+       A class that holds the letters English equivalent i.e Beth = B,V
+       """
+    power = models.CharField(max_length=20)
+    is_active = models.NullBooleanField(null=True, blank=True, default=False)
+
+    def __str__(self):
+        return '{0} - Active: {1}'.format(self.power, self.is_active)
+
+class LetterMeaning(TimeStampedModel):
+    """
+       A class that holds the letters English equivalent i.e Beth = B,V
+       """
+    meaning = models.CharField(max_length=200)
+    is_active = models.NullBooleanField(null=True, blank=True, default=False)
+
+    def __str__(self):
+        return '{0} - Active: {1}'.format(self.meaning, self.is_active)
 
 class Letter(TimeStampedModel):
     title = models.CharField(max_length=200)
+    character = models.CharField(max_length=1)
+    powers = models.ManyToManyField('LetterPower', blank=True)
+    meanings = models.ManyToManyField('LetterMeaning', blank=True)
+    alphabet = models.ForeignKey('Alphabet', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return '{0}: {1} {2}'.format(self.title, self.character, self.alphabet)
+
+class WordMeaning(TimeStampedModel):
+    """
+       A class that holds the letters English equivalent i.e Beth = B,V
+       """
+    meaning = models.CharField(max_length=200)
+    is_active = models.NullBooleanField(null=True, blank=True, default=False)
 
 class Word(TimeStampedModel):
-    title = models.CharField(max_length=200)
+    name_english = models.CharField(max_length=200)
+    name_original_language = models.CharField(max_length=200)
+    letters = models.ManyToManyField('Letter', blank=True)
+    meanings = models.ManyToManyField('WordMeaning', blank=True)
 
 class Dictionary(TimeStampedModel):
     title = models.CharField(max_length=200)
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL , null=True, blank=True)
-    words = models.ManyToManyField('Word', null=True, blank=True)
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, blank=True)
+    words = models.ManyToManyField('Word', blank=True)
+    is_base_dict = models.NullBooleanField(default=False)
+
+class Alphabet(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return '{0} - Lang: {1}'.format(self.title, self.language)
+
+class Language(TimeStampedModel):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class GematriaMethodLetterRule(TimeStampedModel):
+    letter = models.ForeignKey('Letter', on_delete=models.SET_NULL, null=True, blank=True)
+    numerical_value = models.FloatField()
+
+class GematriaMethod(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, blank=True)
+    alphabet = models.ForeignKey('Alphabet', on_delete=models.SET_NULL, null=True, blank=True)
+    letter_rules = models.ManyToManyField('GematriaMethodLetterRule', blank=True)
+
+    def __str__(self):
+        return self.title
 
 
